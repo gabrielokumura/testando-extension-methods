847d4289aaafcadca598d52331ac3fec99c80f3c
Gabrie Okumura <gabrielokumura2017@gmail.com>
2019-10-03 19:31:49 -0300

    Testando

---
+++ ExtensionMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace System
+{
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if (duration.Hours < 24)
+            {
+                return duration.Hours.ToString("F1", CultureInfo.InvariantCulture ) + " Hours";
+            }
+
+            return duration.Days.ToString("F1 " , CultureInfo.InvariantCulture) + " Days";
+
+            
+        }
+
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime dt = new DateTime(2019,10,03,16,10,45);
+
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
